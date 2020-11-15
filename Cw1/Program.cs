c16c940fe4709dac8ec89dcefeeba06687969e8a
Magdalena Tomczak-Kolodzicka <michal.kolodzicki@gmail.com>
2020-11-15 23:50:41 +0100

    Dodatkowe wymagania

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -8,19 +9,39 @@ namespace Cw1
 {
     class Program
     {
-        static void  Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             //example: string remoteUri = "https://blog.home.pl/2015/01/firmowy-adres-e-mail-dlaczego-jest-tak-wazny/";
-            string remoteUri = args[0];
-            Console.WriteLine("Downloading " + remoteUri);
-            byte[] myDataBuffer = new WebClient().DownloadData(remoteUri);
-            string download = Encoding.ASCII.GetString(myDataBuffer);
-            Console.WriteLine("Download successful.");
-            String[] Emails = GetEmailsFromWebContent(download);
-            foreach (String Email in Emails)
-            {
-                Console.WriteLine(Email);
+            string remoteUri = "http://wp.pl";// args[0];
+
+            if (remoteUri == null) {
+                throw new ArgumentNullException("remoteUri", "No URL provided!");
             }
+
+            if (isUrlInvalid(remoteUri)) {
+                throw new ArgumentException("Invalid URL!", "remoteUri");
+            }
+
+            Console.WriteLine("Downloading " + remoteUri);
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetStringAsync(remoteUri);
+                Console.WriteLine("Download successful.");
+                String[] Emails = GetEmailsFromWebContent(response);
+                if (Emails.Length == 0)
+                {
+                    Console.WriteLine("No valid emails found!");
+                } else {
+                    var SetOfEmails = new HashSet<string>(Emails);
+                    foreach (String Email in SetOfEmails)
+                    {
+                        Console.WriteLine(Email);
+                    }
+                }
+                
+
+            }
+ 
         }
 
         private static string[] GetEmailsFromWebContent(string webcontent)
@@ -34,6 +55,11 @@ namespace Cw1
             }
 
             return results;
+        }
+
+        private static bool isUrlInvalid(string remoteUri) 
+        {
+            return !(Uri.IsWellFormedUriString(remoteUri, UriKind.Absolute));
         }
 
     }
