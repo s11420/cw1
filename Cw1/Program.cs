86e0148b66a9f623a81251df69f68807e1bde4a1
Magdalena Tomczak-Kolodzicka <michal.kolodzicki@gmail.com>
2020-11-16 00:07:16 +0100

    Dodano obsługę błędów

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -12,34 +12,42 @@ namespace Cw1
         static async System.Threading.Tasks.Task Main(string[] args)
         {
             //example: string remoteUri = "https://blog.home.pl/2015/01/firmowy-adres-e-mail-dlaczego-jest-tak-wazny/";
-            string remoteUri = "http://wp.pl";// args[0];
+            string remoteUri = args[0];
 
             if (remoteUri == null) {
-                throw new ArgumentNullException("remoteUri", "No URL provided!");
+                throw new ArgumentNullException("remoteUri", "Nie podano adresu URL");
             }
 
             if (isUrlInvalid(remoteUri)) {
-                throw new ArgumentException("Invalid URL!", "remoteUri");
+                throw new ArgumentException("Niepoprawny URL", "remoteUri");
             }
 
-            Console.WriteLine("Downloading " + remoteUri);
-            using (var httpClient = new HttpClient())
+            Console.WriteLine("Pobieranie... " + remoteUri);
+
+            try
             {
-                var response = await httpClient.GetStringAsync(remoteUri);
-                Console.WriteLine("Download successful.");
-                String[] Emails = GetEmailsFromWebContent(response);
-                if (Emails.Length == 0)
+                using (var httpClient = new HttpClient())
                 {
-                    Console.WriteLine("No valid emails found!");
-                } else {
-                    var SetOfEmails = new HashSet<string>(Emails);
-                    foreach (String Email in SetOfEmails)
+                    var response = await httpClient.GetStringAsync(remoteUri);
+                    Console.WriteLine("Download successful.");
+                    String[] Emails = GetEmailsFromWebContent(response);
+                    if (Emails.Length == 0)
                     {
-                        Console.WriteLine(Email);
+                        Console.WriteLine("Nie znaleziono adresów email");
+                    }
+                    else
+                    {
+                        var SetOfEmails = new HashSet<string>(Emails);
+                        foreach (String Email in SetOfEmails)
+                        {
+                            Console.WriteLine(Email);
+                        }
                     }
                 }
-                
-
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Bład w czasie pobierania strony");
             }
  
         }
